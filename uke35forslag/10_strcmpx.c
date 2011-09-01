eb64f3f06b8998cb457a59b07d9044b63d8c010e
Jostein Berre Eliassen <josteinpost@gmail.com>
2011-09-01 22:28:33 +0200

    fix performance complaint

--- uke35forslag/10_strcmpx.c
+++ uke35forslag/10_strcmpx.c
@@ -5,12 +5,14 @@
 
 int strcmpx(unsigned char s1[], unsigned char s2[])
 {
-    int i;
+    int i, len1, len2;
+    len1 = strlen(s1);
+    len2 = strlen(s2);
 
     for (i = 0; ; i++) {
-        if ((i >= strlen(s1)) && i <  strlen(s2)) return -1;
-        if ((i >= strlen(s2)) && i <  strlen(s1)) return 1;
-        if ((i >= strlen(s2)) && i >= strlen(s1)) return 0;
+        if ((i >= len1) && i <  len2) return -1;
+        if ((i >= len2) && i <  len1) return 1;
+        if ((i >= len2) && i >= len1) return 0;
 
         if (s1[i] - s2[i] != 0) return s1[i] - s2[i];
     }
